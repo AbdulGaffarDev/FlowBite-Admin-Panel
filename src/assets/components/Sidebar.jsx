9c010ef85aecc6a83db780a0c1618f3c950e10fb
AbdulGaffarDev <a.gaffar.dev@gmail.com>
2025-06-02 17:51:38 +0500

    Added dark mode functionality to sidebar

--- src/assets/components/Sidebar.jsx
+++ src/assets/components/Sidebar.jsx
@@ -1,4 +1,5 @@
-import React, { useState, useEffect} from "react";
+import React, { useState} from "react";
+import { useSelector } from "react-redux";
 import {
   FaChartPie ,
   FaThLarge,
@@ -10,38 +11,50 @@ import {
 import { GoTriangleRight, GoTriangleDown } from "react-icons/go";
 
 function Sidebar() {
+    const themeState = useSelector(state => state.ui.theme)
     const [activeItem,setActiveItem] = useState(null);
     
     const toggleItem = (label) => {
         setActiveItem(label === activeItem ? null : label);
     }
   return (
-    <div className="">
-    <div className="w-64 h-screen fixed left-0 z-10 bg-white shadow p-4 space-y-2 overflow-auto">
+    <div>
+    <div className={`sidebarContainer ${themeState==='light'? 'bg-white':'bg-blue-950'} `} >
         
-        <div className="flex items-center gap-2 text-[18px]  text-gray-600 hover:bg-gray-100  hover:text-black rounded-md cursor-pointer p-2">
-            <FaChartPie />
-            <span>Dashboard</span>
+        <div className={`sidebarElements 
+                        ${themeState==='light'? 'hover:bg-gray-100':'hover:bg-blue-900'}
+            `}>
+            <FaChartPie className={`${themeState==='light'? 'text-black':'text-white'}`} />
+            <span className={`${themeState==='light'? 'text-black':'text-white'}`} >Dashboard</span>
         </div>
         
         <div>
             <div 
-                className="flex items-center justify-between gap-2 text-[18px] text-gray-600 hover:bg-gray-100  hover:text-black rounded-md cursor-pointer p-2"
+                className={`sidebarElements justify-between 
+                        ${themeState==='light'? 'hover:bg-gray-100':'hover:bg-blue-900'}`}
                 onClick={() => toggleItem('Layouts')}
             >
                 <div className="flex items-center gap-2">
-                    <FaThLarge />
-                    <span>Layouts</span>
+                    <FaThLarge className={`${themeState==='light'? 'text-black':'text-white'}`}/>
+                    <span className={`${themeState==='light'? 'text-black':'text-white'}`}>Layouts</span>
                 </div>
-                <span>{activeItem === "Layouts" ? <GoTriangleDown/> : <GoTriangleRight/>}</span>
+                <span>{activeItem === "Layouts" ? 
+                    <GoTriangleDown className={`${themeState==='light'? 'text-black':'text-white'}`}/> : 
+                    <GoTriangleRight className={`${themeState==='light'? 'text-black':'text-white'}`}/>
+                    }
+                </span>
             </div>
             <div>
                 {activeItem === "Layouts" && (
                     <div className="text-1xl pt-2 text-gray-500">
-                        <div className="p-2 pl-8 hover:bg-gray-100  hover:text-black rounded-md cursor-pointer">
+                        <div className={`${themeState==='light'? 'text-black hover:bg-gray-100':'text-white hover:bg-blue-900'}
+                                        sidebarNestedElements`
+                                        }>
                             Stacked
                         </div>
-                        <div className="p-2 pl-8 hover:bg-gray-100  hover:text-black rounded-md cursor-pointer">
+                        <div className={`${themeState==='light'? 'text-black hover:bg-gray-100':'text-white hover:bg-blue-900'}
+                                        sidebarNestedElements`
+                                        }>
                             Sidebar
                         </div>
                     </div>
@@ -51,22 +64,30 @@ function Sidebar() {
         
         <div>
             <div 
-                className="flex items-center justify-between gap-2 text-[18px] text-gray-600 hover:bg-gray-100  hover:text-black rounded-md cursor-pointer p-2"
+                className={`sidebarElements justify-between 
+                        ${themeState==='light'? 'hover:bg-gray-100':'hover:bg-blue-900'}`}
                 onClick={() => toggleItem('CRUD')}
             >
                 <div className="flex items-center gap-2">
-                    <FaTable />
-                    <span>CRUD</span>
+                    <FaTable className={`${themeState==='light'? 'text-black':'text-white'}`}/>
+                    <span className={`${themeState==='light'? 'text-black':'text-white'}`}>CRUD</span>
                 </div>
-                <span>{activeItem === "CRUD" ? <GoTriangleDown/> : <GoTriangleRight/>}</span>
+                <span>{activeItem === "CRUD" ? 
+                    <GoTriangleDown className={`${themeState==='light'? 'text-black':'text-white'}`}/> : 
+                    <GoTriangleRight className={`${themeState==='light'? 'text-black':'text-white'}`}/>
+                    }
+                </span>
             </div>
             <div>
                 {activeItem === "CRUD" && (
                     <div className="text-1xl pt-2 text-gray-500">
-                        <div className="p-2 pl-8 hover:bg-gray-100  hover:text-black rounded-md cursor-pointer">
-                            Products
+                        <div className={`${themeState==='light'? 'text-black hover:bg-gray-100':'text-white hover:bg-blue-900'}
+                                        sidebarNestedElements`
+                                        }>                            Products
                         </div>
-                        <div className="p-2 pl-8 hover:bg-gray-100  hover:text-black rounded-md cursor-pointer">
+                        <div className={`${themeState==='light'? 'text-black hover:bg-gray-100':'text-white hover:bg-blue-900'} 
+                                        sidebarNestedElements`
+                                        }>
                             Users
                         </div>
                     </div>
@@ -74,35 +95,54 @@ function Sidebar() {
             </div>
         </div>
         
-        <div className="flex items-center gap-2 text-[18px]  text-gray-600 hover:bg-gray-100  hover:text-black rounded-md cursor-pointer p-2">
-            <FaCog />
-            <span>Settings</span>
+        <div className={`sidebarElements 
+                        ${themeState==='light'? 'hover:bg-gray-100':'hover:bg-blue-900'}
+            `}>
+            <FaCog className={`${themeState==='light'? 'text-black':'text-white'}`}/>
+            <span className={`${themeState==='light'? 'text-black':'text-white'}`}>
+                Settings
+            </span>
         </div>
         
         <div>
             <div 
-                className="flex items-center justify-between gap-2 text-[18px] text-gray-600 hover:bg-gray-100  hover:text-black rounded-md cursor-pointer p-2"
+                className={`sidebarElements justify-between 
+                        ${themeState==='light'? 'hover:bg-gray-100':'hover:bg-blue-900'}`}
                 onClick={() => toggleItem('Pages')}
             >
                 <div className="flex items-center gap-2">
-                    <FaChartBar />
-                    <span>Pages</span>
+                    <FaChartBar className={`${themeState==='light'? 'text-black':'text-white'}`}/>
+                    <span className={`${themeState==='light'? 'text-black':'text-white'}`}>
+                        Pages
+                    </span>
                 </div>
-                <span>{activeItem === "Pages" ? <GoTriangleDown/> : <GoTriangleRight/>}</span>
+                <span>{activeItem === "Pages" ? 
+                    <GoTriangleDown className={`${themeState==='light'? 'text-black':'text-white'}`}/> : 
+                    <GoTriangleRight className={`${themeState==='light'? 'text-black':'text-white'}`}/>
+                    }
+                </span>
             </div>
             <div>
                 {activeItem === "Pages" && (
                     <div className="text-1xl pt-2 text-gray-500">
-                        <div className="p-2 pl-8 hover:bg-gray-100  hover:text-black rounded-md cursor-pointer">
+                        <div className={`${themeState==='light'? 'text-black hover:bg-gray-100':'text-white hover:bg-blue-900'} 
+                                        sidebarNestedElements`
+                                        }>
                             Pricing
                         </div>
-                        <div className="p-2 pl-8 hover:bg-gray-100  hover:text-black rounded-md cursor-pointer">
+                        <div className={`${themeState==='light'? 'text-black hover:bg-gray-100':'text-white hover:bg-blue-900'} 
+                                        sidebarNestedElements`
+                                        }>
                             Maintenance
                         </div>
-                        <div className="p-2 pl-8 hover:bg-gray-100  hover:text-black rounded-md cursor-pointer">
+                        <div className={`${themeState==='light'? 'text-black hover:bg-gray-100':'text-white hover:bg-blue-900'} 
+                                        sidebarNestedElements`
+                                        }>
                             404 not found
                         </div>
-                        <div className="p-2 pl-8 hover:bg-gray-100  hover:text-black rounded-md cursor-pointer">
+                        <div className={`${themeState==='light'? 'text-black hover:bg-gray-100':'text-white hover:bg-blue-900'} 
+                                        sidebarNestedElements`
+                                        }>
                             500 server error
                         </div>
                     </div>
@@ -112,28 +152,43 @@ function Sidebar() {
 
         <div>
             <div 
-                className="flex items-center justify-between gap-2 text-[18px] text-gray-600 hover:bg-gray-100  hover:text-black rounded-md cursor-pointer p-2"
+                className={`sidebarElements justify-between 
+                        ${themeState==='light'? 'hover:bg-gray-100':'hover:bg-blue-900'}`}
                 onClick={() => toggleItem('Authentication')}
             >
                 <div className="flex items-center gap-2">
-                    <FaLock />
-                    <span>Authentication</span>
+                    <FaLock className={`${themeState==='light'? 'text-black':'text-white'}`}/>
+                    <span className={`${themeState==='light'? 'text-black':'text-white'}`}>
+                        Authentication
+                    </span>
                 </div>
-                <span>{activeItem === "Authentication" ? <GoTriangleDown/> : <GoTriangleRight/>}</span>
+                <span>{activeItem === "Authentication" ? 
+                    <GoTriangleDown className={`${themeState==='light'? 'text-black':'text-white'}`}/> : 
+                    <GoTriangleRight className={`${themeState==='light'? 'text-black':'text-white'}`}/>
+                    }
+                </span>
             </div>
             <div>
                 {activeItem === "Authentication" && (
                     <div className="text-1xl pt-2 text-gray-500">
-                        <div className="p-2 pl-8 hover:bg-gray-100  hover:text-black rounded-md cursor-pointer">
+                        <div className={`${themeState==='light'? 'text-black hover:bg-gray-100':'text-white hover:bg-blue-900'} 
+                                        sidebarNestedElements`
+                                        }>
                             Sign in
                         </div>
-                        <div className="p-2 pl-8 hover:bg-gray-100      hover:text-black rounded-md cursor-pointer">
+                        <div className={`${themeState==='light'? 'text-black hover:bg-gray-100':'text-white hover:bg-blue-900'} 
+                                        sidebarNestedElements`
+                                        }>
                             Sign up
                         </div>
-                        <div className="p-2 pl-8 hover:bg-gray-100  hover:text-black rounded-md cursor-pointer">
+                        <div className={`${themeState==='light'? 'text-black hover:bg-gray-100':'text-white hover:bg-blue-900'} 
+                                        sidebarNestedElements`
+                                        }>
                             Forgot password
                         </div>
-                        <div className="p-2 pl-8 hover:bg-gray-100  hover:text-black rounded-md cursor-pointer">
+                        <div className={`${themeState==='light'? 'text-black hover:bg-gray-100':'text-white hover:bg-blue-900'} 
+                                        sidebarNestedElements`
+                                        }>
                             Reset password
                         </div>
                     </div>
